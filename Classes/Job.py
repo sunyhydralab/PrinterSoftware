4fb9bd776de6b1b37875e3b3d0868abb1fdc57ea
currymike <currymike@gmail.com>
2023-10-06 10:35:36 -0400

    Job class with printer engine

--- Classes/Job.py
+++ Classes/Job.py
@@ -1,25 +1,39 @@
+import serial
+import serial.tools.list_ports
+import time
+
 # Class for each printer job.
 class Job:
-    def __init__(self, name, file, quantity, position, printers):
-        self.name = name
-        self.file = file
-        self.quantity = quantity
-        self.position = position
-        self.printers = printers
-        self.status = ""
+    # Constructor for the Job class
+    def __init__(self, file, name, quantity, priority, status):
+        self.file = file  # The G-code file
+        self.name = name    # Name of the job
+        self.gcode_lines = self.load_gcode(file)  # List containing the G-code lines
+        self.quantity = quantity  # Quantity of the job
+        self.priority = priority  # Priority of the job
+        self.status = status # Status of the job
         
-        self.extension = ""
-        # If printer is makerbot
-        # self.convertFile()
-        # sets the extension 
-        self.fileType() 
-        # else
-        self.printFile()
+    # Method to load G-code from a given file path
+    def load_gcode(self, path):
+        lines = []
+        with open(path, "r") as g:
+            for line in g:
+                line = line.strip()  # Remove whitespace
+                if len(line) == 0:  # Do not send blank lines
+                    continue
+                if ";" in line: # Remove inline comments
+                    line = line.split(";")[0].strip()  # Remove comments starting with ";"
+                if len(line) == 0 or line.startswith(";"):  # Don't send empty lines and comments
+                    continue
+                lines.append(line)  # Add the G-code line to the list
+        return lines  # Return the list of G-code lines
 
+    # Method to find file type.
     def fileType(self):
         name, ext = os.path.splitext(file)
         self.extension = ext
         
+    # Method for X3G/GCODE conversion.    
     def printFile(self):
         if(self.extension == ".gcode"):
             # send to compatible printer 
