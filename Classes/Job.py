1383ef4ea835236a5f498eeae453b745730843e5
ItBeCharlie <charlied713@gmail.com>
2023-10-16 13:43:58 -0400

    Added pass statements to remove errors

--- Classes/Job.py
+++ Classes/Job.py
@@ -2,17 +2,18 @@ import serial
 import serial.tools.list_ports
 import time
 
+
 # Class for each printer job.
 class Job:
     # Constructor for the Job class
     def __init__(self, file, name, quantity, priority, status):
         self.file = file  # The G-code file
-        self.name = name    # Name of the job
+        self.name = name  # Name of the job
         self.gcode_lines = self.loadGcode(file)  # List containing the G-code lines
         self.quantity = quantity  # Quantity of the job
         self.priority = priority  # Priority of the job
-        self.status = status # Status of the job
-        
+        self.status = status  # Status of the job
+
     # Method to load G-code from a given file.
     def loadGcode(self, file):
         lines = []
@@ -21,9 +22,13 @@ class Job:
                 line = line.strip()  # Remove whitespace
                 if len(line) == 0:  # Do not send blank lines
                     continue
-                if ";" in line: # Remove inline comments
-                    line = line.split(";")[0].strip()  # Remove comments starting with ";"
-                if len(line) == 0 or line.startswith(";"):  # Don't send empty lines and comments
+                if ";" in line:  # Remove inline comments
+                    line = line.split(";")[
+                        0
+                    ].strip()  # Remove comments starting with ";"
+                if len(line) == 0 or line.startswith(
+                    ";"
+                ):  # Don't send empty lines and comments
                     continue
                 lines.append(line)  # Add the G-code line to the list
         return lines  # Return the list of G-code lines
@@ -32,10 +37,12 @@ class Job:
     def fileType(self):
         name, ext = os.path.splitext(file)
         self.extension = ext
-        
-    # Method for X3G/GCODE conversion.    
+
+    # Method for X3G/GCODE conversion.
     def printFile(self):
-        if(self.extension == ".gcode"):
-            # send to compatible printer 
-        elif(self.extension == ".x3g"): 
-            # send to compatible printer 
+        if self.extension == ".gcode":
+            pass
+            # send to compatible printer
+        elif self.extension == ".x3g":
+            pass
+            # send to compatible printer
