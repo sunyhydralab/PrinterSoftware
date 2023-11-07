f900148e9a21c0fc38c0176b06e96f1457bcb6c2
Mike <currym6@newpaltz.edu>
2023-11-07 15:07:32 -0500

    rewrite virtual printer with socat

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import sys
 import os
 
 # Import the classes 
+import serial.tools.list_ports
 from Classes.Job import Job
 from Classes.Printer import Printer
 
+# Current virtual printer implementation does not work.  Rewrite with socat.
 
 if __name__ == '__main__':
 
     # Get the list of available printers. Pass 0 for a list of real connected printers.  Pass 1 for a list of 5 virtual printers.
-    available_printers = Printer.getSupportedPrinters(1)
+    available_printers = Printer.getSupportedPrinters(0)
     # Assuming the first available printer is selected for demo purposes.  
     # This is where the queue would come in.
-    print(len(available_printers))
+    ports = serial.tools.list_ports.comports()
+    available_ports = [port.device for port in ports]
+    print("Available ports: ", available_ports)
      # Create the Job. All this information will come from the front end database.
     test_job = Job("20mm_calibration.gcode", "test", 1, 1, 1)
 
